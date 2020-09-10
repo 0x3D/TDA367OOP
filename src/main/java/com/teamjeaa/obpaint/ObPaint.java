36a1791155ac96e5a28c8a3cf20a5dc821f9be0c
0x3D <0x3D@users.noreply.github.com>
2020-09-10 21:42:45 +0200

    Added a logo

--- src/main/java/com/teamjeaa/obpaint/ObPaint.java
+++ src/main/java/com/teamjeaa/obpaint/ObPaint.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -17,6 +18,7 @@ public final class ObPaint extends Application {
 				getResource("obPaint.fxml")));
 		ResourceBundle obPaintResourceBundle = ResourceBundle.getBundle("obPaint");
 		primaryStage.setTitle(obPaintResourceBundle.getString("application.name"));
+		primaryStage.getIcons().add(new Image("images/logo.png"));
 		primaryStage.setScene(new Scene(root, 600, 600));
 		primaryStage.show();
 	}
