e4b847193326ccfff00a0445524a83e514ddae0a
axelhertzberg <axelher@student.chalmers.se>
2020-09-10 13:23:55 +0200

    JavaFx maven imports done

---
+++ src/main/java/com/teamjeaa/projecttemplate/ObPaint.java
@@ -0,0 +1,17 @@
+package com.teamjeaa.projecttemplate;
+
+import com.teamjeaa.projecttemplate.controller.ProjectController;
+import com.teamjeaa.projecttemplate.model.Project;
+import com.teamjeaa.projecttemplate.view.ProjectView;
+import javafx.scene.canvas.*;
+
+public final class ObPaint {
+	private ObPaint() {
+		/* No instances allowed! */
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
