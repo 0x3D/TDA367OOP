e54f890e09fe39c956e16f0e7d6e06caa3b7bd91
jonasn-chalmers <jonnordi@student.chalmers.se>
2020-09-25 18:34:34 +0200

    Ellipse tests

--- src/main/java/com/teamjeaa/obpaint/model/shapeModel/Mellipse.java
+++ src/main/java/com/teamjeaa/obpaint/model/shapeModel/Mellipse.java
@@ -1,32 +1,37 @@
 package com.teamjeaa.obpaint.model.shapeModel;
 
 public class Mellipse implements Mshape {
-  private final Mpoint centerPoint;
-
-  //Semiaxis for x
-  private final int semiAxesA;
-
-  //Semiaxis for y
-  private final int SemiAxesB;
-
-  Mellipse(Mpoint centerPoint, int semiAxesA, int semiAxesB) {
-    this.centerPoint = centerPoint;
-    this.semiAxesA = semiAxesA;
-    SemiAxesB = semiAxesB;
-  }
-
-  @Override
-  public Mpoint getPosition() {
-    return null;
-  }
-
-  @Override
-  public int getWidth() {
-    return 0;
-  }
-
-  @Override
-  public int getHeight() {
-    return 0;
-  }
+	  private final Mpoint centerPoint;
+	
+	  //Semiaxis for x
+	  private final int semiAxesA;
+	
+	  //Semiaxis for y
+	  private final int semiAxesB;
+	
+	  Mellipse(Mpoint centerPoint, int semiAxesA, int semiAxesB) {
+	    this.centerPoint = centerPoint;
+	    this.semiAxesA = semiAxesA;
+	    this.semiAxesB = semiAxesB;
+	  }
+	
+	  @Override
+	  public Mpoint getPosition() {
+		  return this.centerPoint;
+	  }
+	
+	  @Override
+	  public int getWidth() {
+		  return this.semiAxesA * 2;
+	  }
+	
+	  @Override
+	  public int getHeight() {
+		   return this.semiAxesB * 2;
+	  }
+	
+	  @Override
+	  public Mshape translate(int x, int y) {
+		    return new  Mellipse(new Mpoint(x, y, 0), this.semiAxesA, this.semiAxesB);
+	  }
 }
--- src/main/java/com/teamjeaa/obpaint/model/shapeModel/Mshape.java
+++ src/main/java/com/teamjeaa/obpaint/model/shapeModel/Mshape.java
@@ -4,4 +4,13 @@ public interface Mshape {
   Mpoint getPosition();
   int getWidth();
   int getHeight();
+  
+  
+  /**
+   * 
+   * @param x - int new x pos
+   * @param y - int new y pos
+   * @return New translated Mshape
+   */
+  Mshape translate(int x, int y);
 }
