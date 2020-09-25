247d9f337db960fdb95032b0f2d57f44d2bbd7fd
0x3D <0x3D@users.noreply.github.com>
2020-09-25 18:21:11 +0200

    add functionality to Point and Polygon and tests for these classes

--- src/main/java/com/teamjeaa/obpaint/model/shapeModel/Mpolygon.java
+++ src/main/java/com/teamjeaa/obpaint/model/shapeModel/Mpolygon.java
@@ -2,25 +2,59 @@ package com.teamjeaa.obpaint.model.shapeModel;
 
 import java.util.List;
 
-public class Mpolygon implements Mshape{
+/** This class provides what a polygon is in our model*/
+public class Mpolygon implements Mshape {
   private final List<Mpoint> points;
 
-  private Mpolygon(List<Mpoint> points) {
+  Mpolygon(List<Mpoint> points) {
+    //TODO: This should make a new list
     this.points = points;
   }
 
+  /**
+   * @return the point in the upper left corner, not necessarily on the figure */
   @Override
   public Mpoint getPosition() {
-    return null;
+    return getMinPosition();
   }
 
+  /** @return The point that are in the upper left corner*/
+  private Mpoint getMinPosition() {
+    assert points.get(0)!=null;
+    int minX = points.get(0).getX();
+    int minY = points.get(0).getY();
+    for(Mpoint mpoint:points){
+      minX = Math.min(mpoint.getX(),minX);
+      minY = Math.min(mpoint.getY(),minY);
+    }
+    return new Mpoint(minX,minY);
+  }
+
+  /** @return the point in the lower right corner */
+  private Mpoint getMaxPosition() {
+    assert points.get(0)!=null;
+    int maxX = points.get(0).getX();
+    int maxY = points.get(0).getY();
+    for(Mpoint mpoint:points){
+      maxX = Math.max(mpoint.getX(),maxX);
+      maxY = Math.max(mpoint.getY(),maxY);
+    }
+    return new Mpoint(maxX,maxY);
+  }
+
+  /** @return The width of the rectangle around the polygon*/
   @Override
   public int getWidth() {
-    return 0;
+    Mpoint minPoint = getMinPosition();
+    Mpoint maxPoint = getMaxPosition();
+    return maxPoint.getX()-minPoint.getX();
   }
 
+  /** @return The height of the rectangle around the polygon*/
   @Override
   public int getHeight() {
-    return 0;
+    Mpoint minPoint = getMinPosition();
+    Mpoint maxPoint = getMaxPosition();
+    return maxPoint.getY()-minPoint.getY();
   }
 }
