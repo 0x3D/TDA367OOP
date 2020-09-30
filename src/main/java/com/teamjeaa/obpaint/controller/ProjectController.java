3569e2b3288f2aebd8c7774db558224eb3e8bfcf
0x3D <0x3D@users.noreply.github.com>
2020-09-30 18:32:58 +0200

    remove old code

--- src/main/java/com/teamjeaa/obpaint/controller/ProjectController.java
+++ src/main/java/com/teamjeaa/obpaint/controller/ProjectController.java
@@ -1,6 +1,5 @@
 package com.teamjeaa.obpaint.controller;
 
-import com.teamjeaa.obpaint.model.FileManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 
@@ -8,38 +7,12 @@ import javafx.scene.control.MenuItem;
  * Controller
  */
 public class ProjectController {
-    public final FileManager fileChooser = new FileManager();
     @FXML
     MenuItem fileChoser;
     /** Lite go testning bara */
     @FXML
     private void onFileChoser() {
-        fileChooser.loaderFile();
     }
-  /*
-  public String getColor() {
-  	return cp.getValue().toString();
-  }
-
-  /*
-  public void getMouseXPos () {
-  	rootBorderPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
-  		@Override
-  		public void handle(MouseEvent mouseEvent) {
-  			x1 = mouseEvent.getX();
-  		}
-  	});
-  }
-  public void getMouseYPos () {
-  	rootBorderPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
-  		@Override
-  		public void handle(MouseEvent mouseEvent) {
-  			y1 = mouseEvent.getY();
-  		}
-  	});
-  }
-
-   */
 
 
 
