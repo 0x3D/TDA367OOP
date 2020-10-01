30c7aeac2a87ebc6ffca0d7c1032b176b457df17
erikbengtsson <eribeng@student.chalmers.se>
2020-10-01 17:03:47 +0200

    JavaDoc ShapeFactory & model

--- src/main/java/com/teamjeaa/obpaint/model/Model.java
+++ src/main/java/com/teamjeaa/obpaint/model/Model.java
@@ -1,21 +1,46 @@
 package com.teamjeaa.obpaint.model;
 
+import com.teamjeaa.obpaint.Start;
+import com.teamjeaa.obpaint.controller.CanvasController;
+import com.teamjeaa.obpaint.controller.ToolController;
 import com.teamjeaa.obpaint.model.shapeModel.Mshape;
-import com.teamjeaa.obpaint.model.toolModel.Tool;
+import com.teamjeaa.obpaint.model.toolModel.*;
 import javafx.stage.Stage;
-
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <p>
+ * Responsibility Offers the most vital parts of the model without exposing. Creates ModelCanvas and
+ * holds List with Observers
+ * Used by Start, CanvasController , ConcreteCricleTool, ConcreteRectangleTool, ToolController
+ * Uses Color, List, Observer, Tool
+ * </p>
+ * @author Jonas N
+ * @see CanvasController
+ * @see ConcreteCircleTool
+ * @see ConcreteRectangleTool
+ * @see Start
+ * @see ToolController
+ * @see Color
+ * @see List
+ * @see Observer
+ * @see Tool
+ * @since 0.1-SNAPSHOT
+ */
 public final class Model {
   // Maybe not the static here
+
+
   public static Stage primaryStage;
   private static Color color;
-  private static Model instance;
   private Tool selectedTool;
   private ModelCanvas modelCanvas;
   private List<Observer> observers;
+
+  private static Model instance;
+
 
   private Model() {
     instance = this;
@@ -24,29 +49,14 @@ public final class Model {
     setupModel();
   }
 
-  public static Model getInstance() {
-    if (instance == null) {
-      instance = new Model();
-    }
-    return instance;
-  }
-
-  // TODO Clone
-  public static Color getSelectedColor() {
-    return color;
-  }
-
-  public void setSelectedColor(Color color) {
-    Model.color = color;
-  }
-
   private void setupModel() {
     modelCanvas = new ModelCanvas();
-    // ShapeFactory shapeFactory = new ConcreteShapeFactory();
-    // modelCanvas.addToRender(shapeFactory.createCircle(200, 300, 300));
-    // modelCanvas.addToRender(shapeFactory.createRectangle(0,0,300,300));
   }
 
+  /**
+   * Adds received instence of Mshape to ModelCanvas list for render.
+   * @param s some kind of Mshape.
+   */
   public void addToRender(Mshape s) {
     modelCanvas.addToRender(s);
   }
@@ -56,7 +66,10 @@ public final class Model {
     return selectedTool;
   }
 
-  /** @param selectedTool This method is used to save a tool when the user selects it */
+  /**
+   * This method is used to save a tool when the user selects it. Uses Observer pattern to notify observers.
+   * @param selectedTool Sets the chosen tool to selected for user in order to be able to create som kind of shape.
+   */
   public void setSelectedTool(Tool selectedTool) {
     this.selectedTool = selectedTool;
     notifyObservers();
@@ -68,15 +81,54 @@ public final class Model {
     }
   }
 
+  /**
+   * Singleton pattern. If instance of Model is created it gives requesting class reference to object.
+   * @return intance of the Model.
+   */
+  public static Model getInstance() {
+    if (instance == null) {
+      instance = new Model();
+    }
+    return instance;
+  }
+
+  /**
+   * Observer Pattern. Adds a Observer to the list of observer.
+   * @param observer The Observer that should be notified in case of change.
+   */
   public void addObserver(Observer observer) {
     observers.add(observer);
   }
 
+  /**
+   * Removes choosen Observer from list of Observers. Vill no longer be notified.
+   * @param observer Observer to be removed from list and no longer will be notified in case of change.
+   */
   public void removeObserver(Observer observer) {
     observers.remove(observer);
   }
 
+  /**
+   * @return Returns the list of Mshapes collected from modelCanvas.
+   */
   public List<Mshape> getCanvasShapes() {
     return modelCanvas.getShapes();
+  }
+
+  /**
+   *
+   * @return Returns the JavaAWT Color that is selected from Controller.
+   */
+  // TODO Clone
+  public static Color getSelectedColor() {
+    return color;
+  }
+
+  /**
+   *
+   * @param color Java AWT Color
+   */
+  public void setSelectedColor(Color color) {
+    Model.color = color;
   }
 }
--- src/main/java/com/teamjeaa/obpaint/model/shapeModel/ShapeFactory.java
+++ src/main/java/com/teamjeaa/obpaint/model/shapeModel/ShapeFactory.java
@@ -11,9 +11,27 @@ import java.awt.*;
  * @since 0.1-SNAPSHOT
  */
 public interface ShapeFactory {
+
+  /**
+   * Concrete factory uses method to create a Circle
+   * @param radius Radius of the circle
+   * @param x x value on screen.
+   * @param y y value on screen.
+   * @return returns new instance of the Mellipse (circle) class.
+   */
   Mshape createCircle(int radius, int x, int y, Color color);
 
   // TODO: cant be same have to handle
+
+  /**
+   * Method used by factory to create a Rectangle.
+   * @param x1 X value for point where user starts mouse click.
+   * @param y1 Y value for point where user starts mouse click.
+   * @param x2 X valie for point where user releases mouse.
+   * @param y2 Y value for point where user releases mouse.
+   * @return returns instance of a Mpolygon.
+   */
+
   Mshape createRectangle(int x1, int y1, int x2, int y2, Color color);
 
   // Shape createCircle(int x1, int y1, int radius);
