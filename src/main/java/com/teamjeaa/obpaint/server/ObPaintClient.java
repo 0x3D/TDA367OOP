e2cf5db128e08c341fc7837270219e73a8e5d155
0x3D <0x3D@users.noreply.github.com>
2020-10-19 22:50:19 +0200

    fix

--- src/main/java/com/teamjeaa/obpaint/server/ObPaintClient.java
+++ src/main/java/com/teamjeaa/obpaint/server/ObPaintClient.java
@@ -4,32 +4,37 @@ import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public enum ObPaintClient {
   INSTANCE;
 
-  private PrintWriter out;
-  private Socket socket;
-  private boolean connected=false;
+  private static PrintWriter out;
+  private static Socket socket;
+  private boolean connected = false;
+  private String ip;
+  private int port;
 
-  public void sendCircle(int radius,int centerX,int centerY){
-    out.println("" + radius + "," + centerX + "," + centerY);
-  }
-
-
-  public void connect(String ip, int port) {
+  public void sendCircle(int radius, int centerX, int centerY) {
     try {
       socket = new Socket(ip, port);
       out = new PrintWriter(socket.getOutputStream(), true);
       //       out.printf("Circle: %d %d %10.8f", radius, centerX, centerY);
-      connected=true;
+      connected = true;
+      out.println("" + radius + "," + centerX + "," + centerY);
     } catch (InterruptedIOException interruptedIOException) {
       System.err.println("Remote host timed out");
-      connected=false;
+      connected = false;
     } catch (IOException e) {
       e.printStackTrace();
-      connected=false;
+      connected = false;
     }
+  }
+
+  public void connect(String ip, int port) {
+    this.ip = ip;
+    this.port = port;
+    this.connected = true;
   }
 
   public boolean isConnected() {
