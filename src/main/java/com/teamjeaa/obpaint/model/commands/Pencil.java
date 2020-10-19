3c46c63da36d76f5196a3603ae94159053756417
0x3D <0x3D@users.noreply.github.com>
2020-10-19 20:17:51 +0200

    Remove unnecessary imports

--- src/main/java/com/teamjeaa/obpaint/model/commands/Pencil.java
+++ src/main/java/com/teamjeaa/obpaint/model/commands/Pencil.java
@@ -7,7 +7,6 @@ import com.teamjeaa.obpaint.model.shapeModel.ShapeFactory;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public final class Pencil implements Command {
