16c9656d29c098d2c7ede6fce3e8fa7afa036340
Axel Hertzberg <axelher@student.chalmers.se>
2020-10-21 20:03:59 +0300

    Implemented swedish and english language,axel

--- src/main/java/com/teamjeaa/obpaint/view/MainView.java
+++ src/main/java/com/teamjeaa/obpaint/view/MainView.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
+
 /**
  * View class that loads the fxml file in to a visual Canvas, with all the fxmlFiles included to
  * One. Used by Start.java. Uses Parent.
@@ -11,7 +13,7 @@ import java.io.IOException;
  * @author Jonas N
  */
 public final class MainView extends AnchorPane {
-
+  private static final boolean  SWEDISHSELECTED = true;
   /** Constructor for our view that holds all the logic to load the FXML file */
   public MainView(String fxmlFileName) {
     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFileName));
@@ -19,6 +21,13 @@ public final class MainView extends AnchorPane {
     // fxmlLoader.setController(MainController.getInstance());
 
     try {
+      ResourceBundle resourceBundle;
+      if ( SWEDISHSELECTED) {
+        resourceBundle = ResourceBundle.getBundle("obPaint_sv");
+      }else{
+         resourceBundle = ResourceBundle.getBundle("obPaint");
+      }
+      fxmlLoader.setResources(resourceBundle);
       fxmlLoader.load();
     } catch (IOException exception) {
       throw new RuntimeException(exception);
