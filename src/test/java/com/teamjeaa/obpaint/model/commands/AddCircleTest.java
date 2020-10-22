82f3c4bc71c96bd1c2bc14818fd5455304ef859a
erikbengtsson <eribeng@student.chalmers.se>
2020-10-22 17:19:47 +0200

    Test for Pencil och undo for circle and pencil

--- src/test/java/com/teamjeaa/obpaint/model/commands/AddCircleTest.java
+++ src/test/java/com/teamjeaa/obpaint/model/commands/AddCircleTest.java
@@ -9,15 +9,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddCircleTest {
 
+  Command command;
+
   @Test
   void executeTest() {
     int x = 10;
     int y = 10;
     int radius = 5;
-
-    Command command = new AddCircle(radius, x, y, new Color(255, 175, 175), "test");
+    command = new AddCircle(radius, x, y, new Color(255, 175, 175), "test");
     int size = Model.INSTANCE.getCanvasShapes().size();
     command.execute();
     assertEquals(size + 1, Model.INSTANCE.getCanvasShapes().size());
   }
+
+  @Test
+  void undoTest(){
+    executeTest();
+    int size = Model.INSTANCE.getCanvasShapes().size();
+    command.undo();
+    assertEquals(size - 1, Model.INSTANCE.getCanvasShapes().size());
+  }
 }
+
+
---
+++ src/test/java/com/teamjeaa/obpaint/model/commands/PencilTest.java
@@ -0,0 +1,40 @@
+package com.teamjeaa.obpaint.model.commands;
+
+import com.teamjeaa.obpaint.model.Color;
+import com.teamjeaa.obpaint.model.Model;
+import com.teamjeaa.obpaint.model.shapeModel.ConcreteShapeFactory;
+import com.teamjeaa.obpaint.model.shapeModel.Mpoint;
+import com.teamjeaa.obpaint.model.shapeModel.ShapeFactory;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PencilTest {
+
+    Command command;
+
+
+    @Test
+    void executeTest(){
+        ArrayList<Mpoint> points = new ArrayList<>();
+        points.add(new Mpoint(1,1));
+        points.add(new Mpoint(2,2));
+        points.add(new Mpoint(3,3));
+        points.add(new Mpoint(4,4));
+        command = new Pencil(points, new Color(255, 175, 175), "test",1);
+        int size = Model.INSTANCE.getCanvasShapes().size();
+        command.execute();
+        assertEquals(size + 1, Model.INSTANCE.getCanvasShapes().size());
+    }
+
+    @Test
+    void undoTest(){
+        executeTest();
+        int size = Model.INSTANCE.getCanvasShapes().size();
+        command.undo();
+        assertEquals(size - 1, Model.INSTANCE.getCanvasShapes().size());
+    }
+}
