29e668049e0b147b176dd53d3bd064f140936234
erikbengtsson <eribeng@student.chalmers.se>
2020-10-22 17:44:41 +0200

    Test for removeAllShapes and undo.

---
+++ src/test/java/com/teamjeaa/obpaint/model/commands/RemovaAllShapes.java
@@ -0,0 +1,31 @@
+package com.teamjeaa.obpaint.model.commands;
+
+import com.teamjeaa.obpaint.model.Color;
+import com.teamjeaa.obpaint.model.Model;
+import com.teamjeaa.obpaint.model.shapeModel.ConcreteShapeFactory;
+import com.teamjeaa.obpaint.model.shapeModel.ShapeFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RemovaAllShapes {
+
+    Command command;
+    int size;
+    @Test
+    void removeAllShapes(){
+        ShapeFactory sf = new ConcreteShapeFactory();
+        sf.createRectangle(1,1,10,10,new Color(255, 175, 175),"test");
+        size = Model.INSTANCE.getCanvasShapes().size();
+        command = new RemoveAllShapes();
+        command.execute();
+        assertEquals(0, Model.INSTANCE.getCanvasShapes().size());
+    }
+
+    @Test
+    void undoRemoveAllShapes(){
+        removeAllShapes();
+        command.undo();
+        assertEquals(size,Model.INSTANCE.getCanvasShapes().size());
+    }
+}
